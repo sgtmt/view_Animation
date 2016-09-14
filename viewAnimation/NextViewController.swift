c810c14db881b99876af45c571661a5a0304433c
sgtmt <ainnuen@gmail.com>
2016-09-15 01:58:54 +0900

    add

--- viewAnimation/NextViewController.swift
+++ viewAnimation/NextViewController.swift
@@ -15,9 +15,34 @@ class NextViewController: UIViewController {
         super.viewDidLoad()
         print("ようこそ")
         
+      make_Btn()
+        // ボタンを追加する.
+        
+        let nextButton: UIButton = UIButton(frame: CGRect(x: 0,y: 0, width: 120, height: 50))
+        nextButton.backgroundColor = UIColor.red
+        nextButton.layer.masksToBounds = true
+        nextButton.setTitle("Next", for: .normal)
+        nextButton.layer.cornerRadius = 20.0
+        nextButton.layer.position = CGPoint(x: self.view.bounds.width/2 , y:self.view.bounds.height-50)
+        nextButton.addTarget(self, action: #selector(onClickMyButton(sender:)), for: .touchUpInside)
+
+        self.view.addSubview(nextButton);
+        
         // Do any additional setup after loading the view.
     }
-    override func viewDidAppear(animated: Bool) {
+    internal func onClickMyButton(sender: UIButton){
+        
+        // 遷移するViewを定義する.
+        let mySecondViewController: UIViewController = NextViewController()
+        
+        // アニメーションを設定する.
+        mySecondViewController.modalTransitionStyle = .partialCurl
+        
+        // Viewの移動する.
+        self.present(mySecondViewController, animated: true, completion: nil)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
         
         // 戻る場合には、dismissViewControllerAnimatedメソッドを使います。
         // ここで、animatedをtrueにするとアニメーションしながら遷移できる
@@ -25,9 +50,12 @@ class NextViewController: UIViewController {
         let str = "atai;"
         atai = atai+1
         print(str + " \(atai)")
-        self.dismissViewControllerAnimated(true, completion: nil)
+        self.dismiss(animated: true, completion: nil)
     }
 
+    func make_Btn(){
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
